5c00ff62bf7fe4513c0cdcd7a9c106565391eb9a
Austin Zambito-Valente <azv04@stanford.edu>
2020-05-21 02:40:43 -0400

    Initial Commit

---
+++ Solitaire/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Solitaire
+//
+//  Created by Austin Zambito-Valente on 5/21/20.
+//  Copyright © 2020 Austin Zambito-Valente. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
